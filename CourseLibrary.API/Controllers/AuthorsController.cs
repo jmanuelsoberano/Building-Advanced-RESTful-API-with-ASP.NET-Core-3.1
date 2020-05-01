1fbd1d754124d57d8e15ae61a1998045e6ec0d45
Jose Manuel Campos Soberano <jmcsoberano@gmail.com>
2020-05-01 09:44:09 -0500

    Soporte HATEOAS para un solo recurso

--- CourseLibrary.API/Controllers/AuthorsController.cs
+++ CourseLibrary.API/Controllers/AuthorsController.cs
@@ -88,14 +88,22 @@ namespace CourseLibrary.API.Controllers
                 return BadRequest();
             }
 
-            var authorsFromRepo = _courseLibraryRepository.GetAuthor(authorId);
+            var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
 
-            if (authorsFromRepo == null)
+            if (authorFromRepo == null)
             {
                 return NotFound();
             }
 
-            return Ok(_mapper.Map<AuthorDto>(authorsFromRepo).ShapeData(fields));
+            var links = CreateLinksForAuthor(authorId, fields);
+
+            var linkedResourceToReturn =
+                _mapper.Map<AuthorDto>(authorFromRepo).ShapeData(fields)
+                as IDictionary<string, object>;
+
+            linkedResourceToReturn.Add("links", links);
+
+            return Ok(linkedResourceToReturn);
         }
 
         [HttpPost]
@@ -118,7 +126,7 @@ namespace CourseLibrary.API.Controllers
             return Ok();
         }
 
-        [HttpDelete("{authorId}")]
+        [HttpDelete("{authorId}", Name = "DeleteAuthor")]
         public ActionResult DeleteAuthor(Guid authorId)
         {
             var authorFromRepo = _courseLibraryRepository.GetAuthor(authorId);
@@ -176,5 +184,43 @@ namespace CourseLibrary.API.Controllers
                         });
             }
         }
+
+        private IEnumerable<LinkDto> CreateLinksForAuthor(Guid authorId, string fields)
+        {
+            var links = new List<LinkDto>();
+
+            if (string.IsNullOrWhiteSpace(fields))
+            {
+                links.Add(
+                    new LinkDto(Url.Link("GetAuthor", new { authorId }),
+                    "self",
+                    "GET"));
+            }
+            else
+            {
+                links.Add(
+                    new LinkDto(Url.Link("GetAuthor", new { authorId, fields }),
+                    "self",
+                    "GET"));
+            }
+
+            links.Add(
+                new LinkDto(Url.Link("DeleteAuthor", new { authorId }),
+                "delete_author",
+                "DELETE"));
+
+            links.Add(
+                new LinkDto(Url.Link("CreateCourseForAuthor", new { authorId }),
+                "create_course_for_author",
+                "POST"));
+
+            links.Add(
+                new LinkDto(Url.Link("GetCoursesForAuthor", new { authorId }),
+                "courses",
+                "GET"));
+
+            return links;
+        }
+
     }
 }
--- CourseLibrary.API/Controllers/CoursesController.cs
+++ CourseLibrary.API/Controllers/CoursesController.cs
@@ -30,7 +30,7 @@ namespace CourseLibrary.API.Controllers
                 throw new ArgumentNullException(nameof(mapper));
         }
 
-        [HttpGet]
+        [HttpGet( Name = "GetCoursesForAuthor" )]
         public ActionResult<IEnumerable<CourseDto>> GetCoursesForAuthor(Guid authorId)
         {
             if (!_courseLibraryRepository.AuthorExists(authorId))
@@ -60,7 +60,7 @@ namespace CourseLibrary.API.Controllers
             return Ok(_mapper.Map<CourseDto>(courseForAuthorFromRepo));
         }
 
-        [HttpPost]
+        [HttpPost( Name = "CreateCourseForAuthor" )]
         public ActionResult<CourseDto> CreateCourseForAuthor(
             Guid authorId, CourseForCreationDto course)
         {
